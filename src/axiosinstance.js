455df94ea466923b22f0670e2c9e6c91f695764b
Ankit Nayan <ankitnayan83@gmail.com>
2023-04-18 18:55:56 +0530

    commit

--- src/axiosinstance.js
+++ src/axiosinstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:8080/api";
+
 axios.defaults.withCredentials = true;
 export const publicRequest = axios.create({
    baseURL: BASE_URL,
