2cde64dfd77129311a81fc90f644bff46e691ec9
Ankit Nayan <ankitnayan83@gmail.com>
2023-04-21 17:48:07 +0530

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -32,10 +32,6 @@ export const Home = () => {
                <Header />
             </>
          )}
-         <>
-            <Navbar />
-            <Header />
-         </>
       </div>
    );
 };
